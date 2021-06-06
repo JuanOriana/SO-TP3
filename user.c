75fdbe939e32b51f69e618b4c18920c698b8cea7
Juan Oriana <joriana@itba.edu.ar>
2021-06-06 16:13:06 -0300

    Fix input

--- user.c
+++ user.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
     while (fgets(buffer, BUFF_LEN - 1, stdin) != NULL)
     {
         /* Send message to the server */
-        int len = write(socketFd, buffer, BUFF_LEN);
+        int len = write(socketFd, buffer, strlen(buffer));
 
         if (len < 0)
         {
